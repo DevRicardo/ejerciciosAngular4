32c13a79e834cc61fdc661186d5fc955a0ac622d
Jose A. Ricardo <jricardo@avanzamas.com.co>
2017-07-19 08:17:45 -0500

    ejercicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+      id:   1,
+      name: 'windStorm'
+  } 
+  heroes = HEROES
+  selectedHero: Hero;
+
+  onSelect(hero: Hero):void{
+    this.selectedHero = hero;
+  }
+  
+
 }
+
+
+export class Hero{
+  id: number;
+  name: string;
+}
+
+
+
+const HEROES: Hero[] = [
+
+  {id: 11, name: "Jose Ricardo"},
+  {id: 12, name: "Jose Ricardo 1"},
+  {id: 13, name: "Jose Ricardo 2"},
+  {id: 14, name: "Jose Ricardo 3"},
+  {id: 15, name: "Jose Ricardo 4"},
+  {id: 16, name: "Jose Ricardo 5"},
+  {id: 17, name: "Jose Ricardo 6"},
+  {id: 18, name: "Jose Ricardo 7"},
+  {id: 19, name: "Jose Ricardo 8"}
+]
+
