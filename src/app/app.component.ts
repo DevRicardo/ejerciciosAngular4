fddd5215fca65a98d38e07dc68b24b66c91929bb
Jose A. Ricardo <ricardoingenierias@gmail.com>
2017-07-20 23:01:12 -0500

    Separando hero detail en un componente independiente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero }      from './hero';
 
 @Component({
   selector: 'app-root',
@@ -20,14 +21,6 @@ export class AppComponent {
   
 
 }
-
-
-export class Hero{
-  id: number;
-  name: string;
-}
-
-
 
 const HEROES: Hero[] = [
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { DetailComponent } from './detail.components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
