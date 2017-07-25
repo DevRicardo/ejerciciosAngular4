54b8587faebdd1d21bea492e91170c2ea24709ce
Jose A. Ricardo <ricardoingenierias@gmail.com>
2017-07-24 21:58:34 -0500

    [DEV] se crear un servicio para consumir el listado de heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { Hero }      from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Tour of Heroes';
-  hero: Hero = {
-      id:   1,
-      name: 'windStorm'
-  } 
-  heroes = HEROES
+   
+  constructor(private heroService: HeroService){
+
+  }  
+  heroes: Hero[];
   selectedHero: Hero;
 
+  ngOnInit(): void{
+    this.getHeroes();
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroesSlowly().then(heroes => 
+      this.heroes = heroes
+    );
+  }
   onSelect(hero: Hero):void{
     this.selectedHero = hero;
   }
   
 
 }
-
-const HEROES: Hero[] = [
-
-  {id: 11, name: "Jose Ricardo"},
-  {id: 12, name: "Jose Ricardo 1"},
-  {id: 13, name: "Jose Ricardo 2"},
-  {id: 14, name: "Jose Ricardo 3"},
-  {id: 15, name: "Jose Ricardo 4"},
-  {id: 16, name: "Jose Ricardo 5"},
-  {id: 17, name: "Jose Ricardo 6"},
-  {id: 18, name: "Jose Ricardo 7"},
-  {id: 19, name: "Jose Ricardo 8"}
-]
-
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+
+
+export const HEROES: Hero[] = [
+
+  {id: 11, name: "Jose Ricardo"},
+  {id: 12, name: "Jose Ricardo 1"},
+  {id: 13, name: "Jose Ricardo 2"},
+  {id: 14, name: "Jose Ricardo 3"},
+  {id: 15, name: "Jose Ricardo 4"},
+  {id: 16, name: "Jose Ricardo 5"},
+  {id: 17, name: "Jose Ricardo 6"},
+  {id: 18, name: "Jose Ricardo 7"},
+  {id: 19, name: "Jose Ricardo 8"}
+]
